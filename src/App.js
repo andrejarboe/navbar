12d2f0e153db45959817278a683287233da7bfc0
Andre Jarboe <andrejarboe@gmail.com>
2023-01-29 13:54:28 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import Navbar from './Navbar';
-import Navbar2 from './Navbar2';
-import HeroSlider from './HeroSlider';
 
-import './main.css'
+import './main.css';
 
 function App() {
 	return (
 		<>
-			{/* <Navbar /> */}
-			<Router>
-				<Navbar2 />
-				<HeroSlider />
-			</Router>
+			<Navbar />
 		</>
 	);
 }
